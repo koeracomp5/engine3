3b103716228a444929e7c0005d8994862ec784a6
shoulderMaster <ckdqja0225@gmail.com>
2020-01-22 11:30:30 +0900

    implement next scan

--- next_scan_test.py
+++ next_scan_test.py
@@ -2,6 +2,7 @@
 import winappdbg
 import inspect
 import os
+import struct
 
 
 
@@ -20,7 +21,8 @@ class Hack(object):
 
     def get_memory_full_dump_db(self) :
         mem_dump_db = dict()
-        for base_addr, size in self.vaddr_map :
+        for base_addr in self.vaddr_map.keys() :
+            size = self.vaddr_map[base_addr]
             mem_dump_db[base_addr] = self.memory_read(base_addr, size)
         return mem_dump_db
 
@@ -56,11 +58,11 @@ class Hack(object):
         print "total commited memory size : %dbytes" % (commited_page_cnt*(2**12))
 
     def get_vaddr_map(self) :
-        vaddr_map = []
+        vaddr_map = dict()
         total_size = 0
         for mbi in self.hwnd.get_memory_map() :
             if mbi.is_commited() and mbi.is_writeable() and not mbi.is_guard() :
-                vaddr_map.append( (mbi.BaseAddress, mbi.RegionSize) )
+                vaddr_map[mbi.BaseAddress] = mbi.RegionSize
                 total_size += mbi.RegionSize
         print("total non-protected writeable page size : %dbytes" % total_size)
         return vaddr_map
@@ -79,15 +81,66 @@ class Hack(object):
                     if name == processName:
                         return process
 
-    def next_scan(self, oper_type) :
+    def next_scan(self, oper_func) :
+        var_db = dict()
+        next_full_dump_db = dict()
+        var_size = 4
         if self.variable_info_db == None :
             next_full_dump_db = self.get_memory_full_dump_db()
-            for base
+            for base_addr in self.memory_dump_db.keys() :
+                var_db[base_addr] = []
+            for base_addr in self.memory_dump_db.keys() :
+                size = self.vaddr_map[base_addr]
+                offset = 0
+                while offset+var_size < size :
+                    origin_val = struct.unpack("i", self.memory_dump_db[base_addr][offset:offset+var_size])[0]
+                    next_val = struct.unpack("i", next_full_dump_db[base_addr][offset:offset+var_size])[0]
+                    if oper_func(origin_val, next_val) :
+                        var_db[base_addr].append(offset)
+                    offset+=1
+        else :
+            for base_addr in self.variable_info_db.keys() :
+                size = self.vaddr_map[base_addr]
+                next_dump = self.memory_read(base_addr, size)
+                var_db_entry = []
+                for offset in self.variable_info_db[base_addr] :
+                    origin_val = struct.unpack("i", self.memory_dump_db[base_addr][offset:offset+var_size])[0]
+                    next_val = struct.unpack("i", next_dump[offset:offset+var_size])[0]
+                    if oper_func(origin_val, next_val) :
+                        var_db_entry.append(offset)
+                if len(var_db_entry) > 0 :
+                    var_db[base_addr] = var_db_entry
+                    next_full_dump_db[base_addr] = next_dump
+        self.memory_dump_db = next_full_dump_db
+        self.variable_info_db = var_db
+
+    def print_scaned_var_info (self) :
+        var_size = 4
+        var_cnt = 0
+        for base_addr in self.variable_info_db.keys() :
+            for offset in self.variable_info_db[base_addr] :
+                addr = base_addr+offset
+                val = struct.unpack("i", self.memory_dump_db[base_addr][offset:offset+var_size])[0]
+                print "0x%08x -> %d" % (addr, val)
+                var_cnt+=1
+        print "total %d variables scaned" % var_cnt
 
 
 def main () :
     ps = Hack("KakaoTalk.exe")
     ps.first_scan()
+    cmd_str = raw_input()
+    while "fuck" not in cmd_str :
+        if "inc" in cmd_str :
+            ps.next_scan(lambda a,b : a<b)
+        elif "dec" in cmd_str :
+            ps.next_scan(lambda a,b : a>b)
+        else :
+            ps.next_scan(lambda a,b : a==b)
+        ps.print_scaned_var_info()
+        cmd_str = raw_input()
+
+
     #ps = Hack("notepad.exe")
 
 
